669e846217b6245c5c09bc4248207ffd30ce08e3
TreMartin-OS <tremartin3003@gmail.com>
2024-07-05 16:21:54 -0500

    ADD Google Places API key to .env and .env.example. ADD Planner.jsx, PlannerDisplay.jsx, and PlannerSearch.jsx.

---
+++ client/src/PlannerDisplays.jsx
@@ -0,0 +1,8 @@
+/* 
+This page is for displaying pulled saved plans from the DB
+
+Methods:
+POST request to create a new Plan
+PATCH request to change Plan name
+DELETE request to delete an existing Plan
+*/
---
+++ client/src/PlannerSearch.jsx
@@ -0,0 +1,8 @@
+/* 
+This Page will:
+GET Search results from API - from Server
+
+Methods: 
+POST request to DB 
+
+*/
