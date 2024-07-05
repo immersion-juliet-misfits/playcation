ff99f7d11aa0e4fab26907d576ca63ab6f053881
Gage <gagecantrelle1899@gmail.com>
2024-07-05 10:29:23 -0500

    added fake data for testing

---
+++ server/fakeData.js
@@ -0,0 +1,28 @@
+let reviews = [{id: 'bob', text: 'hey there I like this place', stars: 5}, {id: 'boo', text: 'hey there I hate this place', stars: 0},  
+  {id: 'someone', text: 'nice place', stars: 1}, {id: 'boo', text: 'bad place', stars: 2},
+  {id: 'cat', text: 'hey there I am a cat', stars: 3},  {id: 'dog', text: 'hey there I am a dog', stars: 4}
+];
+
+let vacationPlace = [
+  {id: 1234,
+  location: '1234Street',
+  name: 'fun Holetale',
+  reviews: reviews,
+  image: 'image'
+},
+{id: 1234,
+  location: 'florda',
+  name: 'Disany world',
+  reviews: reviews,
+  image: 'image'
+},
+{id: 1234,
+  location: '1211 Avanue',
+  name: 'bayou camperPark',
+  reviews: reviews,
+  image: 'image'
+}
+
+]
+
+export {reviews, vacationPlace}
