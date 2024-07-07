25d4ef56304f750fb0439cfa06b69b6d348609b3
ccaldwell11 <camroncldwll@gmail.com>
2024-07-06 19:50:14 -0500

    Add Homepage (previously forgotten)

---
+++ client/src/components/Homepage.jsx
@@ -0,0 +1,14 @@
+import React, { Component, useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
+import { Button, Topography, Box } from '@mui/material';
+
+const Homepage = () => {
+  return (
+    <div>
+      <h1>Homepage</h1>
+    </div>
+  );
+};
+
+export default Homepage;
