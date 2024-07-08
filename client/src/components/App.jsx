4dbb4d6465b06aeb087da91865a56f5619f96576
Gage <gagecantrelle1899@gmail.com>
2024-07-08 15:53:20 -0500

    worked on set up for reviews

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,7 +1,7 @@
 import React, { useState, Component } from "react";
 import { Routes, Route, Link } from "react-router-dom";
 import Homepage from "./Homepage.jsx";
-import Reviews from "./review/Reviews.jsx"
+import Reviews from "./reviews/Reviews.jsx"
 
 // ES6 Class/Functional component
 const App = () => {
@@ -14,7 +14,6 @@ const App = () => {
         <Route path="/" element={<Homepage />} />
         <Route path="/home" element={<Homepage />} />
         <Route path="/*" element={<h1>Page Not Found</h1>} />
-        <Route path="/review" element={<Reviews />} />
       </Routes>
     </>
   );
