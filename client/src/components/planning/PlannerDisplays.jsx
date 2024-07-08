4e03f041abf152e71ec4196c173dc7ae1d433b0d
TreMartin-OS <tremartin3003@gmail.com>
2024-07-08 18:26:53 -0500

    Updated Planner.jsx by moving inner Grid elements to PlannerSearch and Planner Displays. Passed those components to Planner. Left in commented-out code involving using the Container untile a decision is made on site layout.

--- client/src/components/planning/PlannerDisplays.jsx
+++ client/src/components/planning/PlannerDisplays.jsx
@@ -6,3 +6,23 @@ POST request to create a new Plan
 PATCH request to change Plan name
 DELETE request to delete an existing Plan
 */
+
+import { Link } from 'react-router-dom';
+import React from 'react';
+import axios from 'axios';
+import { Grid, Paper, TextField } from '@mui/material';
+
+
+const PlannerDisplays = () => {
+  // React Hooks replace state, constructor, super(), & bind
+
+  return (
+        <Grid className='grid_plans' item xs={6} style={{ midWidth: '200px' }}>
+          <Paper style={{ padding: 16, height: '100%' }}>
+            <div><h1>Saved Plans Component Here</h1></div>
+          </Paper>
+        </Grid>
+  );
+};
+
+export default PlannerDisplays;
--- client/src/components/planning/PlannerSearch.jsx
+++ client/src/components/planning/PlannerSearch.jsx
@@ -6,3 +6,24 @@ Methods:
 POST request to DB 
 
 */
+
+import { Link } from 'react-router-dom';
+import React from 'react';
+import axios from 'axios';
+import { Grid, Paper, TextField } from '@mui/material';
+
+
+const PlannerSearch = () => {
+  // React Hooks replace state, constructor, super(), & bind
+
+  return (
+        <Grid className='grid_search' item xs={6} style={{ midWidth: '200px' }}>
+          <Paper style={{ padding: 16, height: '100%' }}>
+            <h1>Search Component Here</h1>
+            <TextField variant='outlined' placeholder='Search Here' fullWidth />
+          </Paper>
+        </Grid>
+  );
+};
+
+export default PlannerSearch;
