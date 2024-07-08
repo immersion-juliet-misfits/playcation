cf471b1c24de3a18cc3a3fc0285b5c489143586a
TreMartin-OS <tremartin3003@gmail.com>
2024-07-08 15:46:08 -0500

    Feature / NavDrawer added. Files updated for React Hooks.

--- client/src/components/planning/Planner.jsx
+++ client/src/components/planning/Planner.jsx
@@ -5,49 +5,47 @@ import React from 'react';
 import axios from 'axios';
 import { Container, Grid, Paper, TextField } from '@mui/material';
 
-class Planner extends React.Component {
-  constructor(props) {
-    super();
-    this.state = {
-    };
-    // Bind methods here
-  }
+const Planner = () => {
+  // React Hooks replace state, constructor, and super()
+  // Don't have to bind methods
 
   // GET Method to retrieve stored plans
-  // getPlans(){
+  // getPlans(){};
 
+  // Mount previous method on page load - Does React Hooks use this?
+  // componentDidMount() {
+  //   this.getPlans()
   // };
 
-  // Mount previous method on page load
-// componentDidMount() {
-//   this.getPlans()
-// };
-
-  // POST Method to create new plans 
-  // savePlans() {
-
-  // };
+  // POST Method to create new plans
+  // savePlans() {};
 
   // Render required Planner Components
-  render() {
-    return (
-      <Container className="planner_all" style={{ width: '800vh', height: '80vh', border: '2px solid black', borderRadius: '20px'}}>
-        <Grid className="grid_all" container spacing={2}  alignItems="stretch">
-          <Grid className="grid_search" item xs={6}>
-            <Paper style={{ padding: 16, height: '100%' }}>
-              <h1>Search Component Here</h1>
-              <TextField variant='outlined' placeholder='Search Here' fullWidth />
-            </Paper>
-          </Grid>
-          <Grid className="grid_plans" item xs={6}>
-            <Paper style={{ padding: 16, height: '100%'  }}>
-              <div>Saved Plans Component Here</div>
-            </Paper>
-          </Grid>
+  return (
+    <Container
+      className='planner_all'
+      style={{
+        width: '800vh',
+        height: '80vh',
+        border: '2px solid black',
+        borderRadius: '20px',
+      }}
+    >
+      <Grid className='grid_all' container spacing={2} alignItems='stretch'>
+        <Grid className='grid_search' item xs={6}>
+          <Paper style={{ padding: 16, height: '100%' }}>
+            <h1>Search Component Here</h1>
+            <TextField variant='outlined' placeholder='Search Here' fullWidth />
+          </Paper>
         </Grid>
-      </Container>
-    );
-  }
-}
+        <Grid className='grid_plans' item xs={6}>
+          <Paper style={{ padding: 16, height: '100%' }}>
+            <div>Saved Plans Component Here</div>
+          </Paper>
+        </Grid>
+      </Grid>
+    </Container>
+  );
+};
 
 export default Planner;
