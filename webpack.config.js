952a4c4ab17509c283a188ff2c3ca3f09bdc3761
ccaldwell11 <camroncldwll@gmail.com>
2024-07-08 09:11:20 -0500

    Add route fallbacks for React Rputer compatability

--- webpack.config.js
+++ webpack.config.js
@@ -14,7 +14,7 @@ module.exports = {
     rules: [
       {
         test: /\.(js|jsx)$/,
-        use: {loader: 'babel-loader', options:{
+        use: {loader: 'babel-loader', options: {
           presets: ['@babel/preset-env', '@babel/preset-react']
         }},
         exclude: /node_modules/,
