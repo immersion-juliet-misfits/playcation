9b294b2bfa25304ad055bae749f69cc1790b8034
ccaldwell11 <camroncldwll@gmail.com>
2024-07-09 14:31:38 -0500

    Update CommunityPage component to show form option1

--- client/src/components/community/CommunityPage.jsx
+++ client/src/components/community/CommunityPage.jsx
@@ -4,7 +4,21 @@ import axios from "axios";
 
 const CommunityPage = () => {
   return (
-    <h2>Sharecation</h2>
+    <div>
+    <h2>Playcay Ventures</h2>
+    <form>
+      <label htmlFor="commtitle">Venture Location:</label><br/>
+      <input id="commtitle" type="text" placeholder="Where'd you go?"/><br/><br/>
+
+      <label htmlFor="commbody">Venture Story:</label><br/>
+      <textarea id="commtitle" type="text" placeholder="Share your experience" /><br/><br/>
+      
+      <label htmlFor="comm">Venture Snapshot:</label><br/>
+      <input id="commtitle" type="file"></input><br/><br/>
+      
+      <input type="submit" value="Post Venture" />
+    </form>
+    </div>
   )
 }
 
