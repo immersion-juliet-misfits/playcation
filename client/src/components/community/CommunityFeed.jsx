1ac7700785669834a7d90a8fa8a4fc0609b4378b
ccaldwell11 <camroncldwll@gmail.com>
2024-07-09 15:27:55 -0500

    Add CommunityFeed + CommunityInput component/isolation of components originating from CommunityPage

---
+++ client/src/components/community/CommunityFeed.jsx
@@ -0,0 +1,22 @@
+import React, { useState } from "react";
+import axios from "axios";
+
+
+const CommunityFeed = () => {
+  return (
+    <div>
+    <h2>Playcay Daily Feed</h2><button>Make a Post</button>
+    <ul>
+        <li>Post 1</li>
+        <li>Post 2</li>
+        <li>Post 3</li>
+        <li>Post 4</li>
+        <li>Post 5</li>
+        <li>Post 6</li>
+        <li>Post 7</li>
+    </ul>
+    </div>
+  )
+}
+
+export default CommunityFeed;
