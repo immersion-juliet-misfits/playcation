44fc58635ae4b4ca6a35c899ce922540e38cf6d7
Darius <kudoreha9@gmail.com>
2024-07-09 16:58:26 -0400

    Added basic layout for the Watchout compoenent. Tested infomration to render on page.

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -5,7 +5,7 @@ import Planner from "./planning/Planner.jsx"
 import NavDrawer from "./NavDrawer.jsx"
 // import CommunityPage from "./community/CommunityPage.jsx"
 // import Reviews from "./reviews/Reviews.jsx"
-// import Watchout from "./threats/Watchout.jsx"
+import Watchout from "./threats/Watchout.jsx"
 
 // ES6 Class/Functional component
 const App = () => {
@@ -18,7 +18,7 @@ const App = () => {
         <Route path="/planner" element={<Planner />} />
         {/* <Route path="/community" element={<CommunityPage />} /> */}
         {/* <Route path="/reviews" element={<Reviews />} /> */}
-        {/* <Route path="/watchout" element={<Watchout />} /> */}
+        <Route path="/watchout" element={<Watchout />} />
         <Route path="/*" element={<h1>Page Not Found</h1>} />
       </Routes>
       </> 
