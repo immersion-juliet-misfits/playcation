3e0f1627439495bdbf124b7b9d2b5d6f87375e5c
ccaldwell11 <camroncldwll@gmail.com>
2024-07-09 12:01:24 -0500

    Add request handler for communityPosts GET

--- server/routes/communityRoutes/community.js
+++ server/routes/communityRoutes/community.js
@@ -1,12 +1,21 @@
 const express = require('express');
 const Community = express.Router();
+const { communityPost } = require('../../db/index');
 
 Community.use(express.json());
 
 // express handles request; db models + sequelize handle querying
 Community.get('/post', (req, res) => {
-  res.send('good try');
+  communityPost.findAll()
+    .then((data) => {
+      console.log(data[1].dataValues);
+      res.send(data);
+    });
 });
+
+// Community.post('/post', (req, res) => {
+//   communityPost.
+// })
 //   .then((tr) => {
 //     res.send('good try', tr)
 //   })
