30089bec061a4d9c4cd3c3b09af80e7720f53bb9
ccaldwell11 <camroncldwll@gmail.com>
2024-07-09 15:03:49 -0500

    Update CommunityPage component for various form entries

--- client/src/components/community/CommunityPage.jsx
+++ client/src/components/community/CommunityPage.jsx
@@ -18,6 +18,33 @@ const CommunityPage = () => {
       
       <input type="submit" value="Post Venture" />
     </form>
+
+    <h2>Playcay Plans</h2>
+    <form>
+      <label htmlFor="commtitle">Venture Location:</label><br/>
+      <input id="commtitle" type="text" placeholder="Where'd you go?"/><br/><br/>
+      <label htmlFor="commtitle">Venture Stop:</label><br/>
+      <input id="commtitle" type="text" placeholder="A place worth visiting"/><br/>
+      <textarea id="commtitle" maxLength="111" type="text" placeholder="Short description" /><br/>
+      <input type="button" value="Add Stop" /><br/><br/>
+
+            
+      <input type="submit" value="Post Plan" />
+    </form>
+
+    <h2>Playcay Fun</h2>
+    <form>
+      <label htmlFor="commtitle">Venture Location:</label><br/>
+      <input id="commtitle" type="text" placeholder="Where'd you go? (this will be hidden)"/><br/><br/>
+      <label htmlFor="commtitle">Venture Hint 1:</label><br/>
+      <textarea id="commtitle" maxLength="50" type="text" placeholder="Short hint 1" /><br/>
+      <label htmlFor="commtitle">Venture Hint 2:</label><br/>
+      <textarea id="commtitle" maxLength="50" type="text" placeholder="Short hint 2" /><br/>
+      <label htmlFor="commtitle">Venture Hint 3:</label><br/>
+      <textarea id="commtitle" maxLength="50" type="text" placeholder="Short hint 3" /><br/>
+            
+      <input type="submit" value="Post Fun" />
+    </form>
     </div>
   )
 }
