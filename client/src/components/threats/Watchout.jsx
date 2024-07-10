d1cea96f18e6cb0329e8f0f6850d068d03e2c409
Darius <kudoreha9@gmail.com>
2024-07-09 20:46:05 -0400

    Making plan for how real data will be handled in component.

--- client/src/components/threats/Watchout.jsx
+++ client/src/components/threats/Watchout.jsx
@@ -1,110 +1,28 @@
-// Currently messing with new API that has been found
-// Import react
-import React from "react"
+// Import react hooks
+// Will need useEffect, and useState
+ import React, { useEffect, useState } from 'react';
 
-// Dummy data here
-const weatherObj = [
-    {
-    "country_code":"US",
-    "lon":-84.19161,
-    "timezone":"America\/New_York",
-    "lat":39.75895,
-    "alerts":[
-       {
-          "regions":[
-             "Madison",
-             " Franklin",
-             " Scioto",
-             " Hardin",
-             " Logan",
-             " Licking",
-             " Montgomery",
-             " Clermont",
-             " Fayette",
-             " Brown",
-             " Mercer",
-             " Wayne",
-             " Adams",
-             " Union",
-             " Warren",
-             " Highland",
-             " Lewis",
-             " Ross",
-             " Robertson",
-             " Clark",
-             " Fayette",
-             " Preble",
-             " Shelby",
-             " Clinton",
-             " Franklin",
-             " Pike",
-             " Miami",
-             " Delaware",
-             " Bracken",
-             " Greene",
-             " Auglaize",
-             " Hocking",
-             " Fairfield",
-             " Butler",
-             " Champaign",
-             " Pickaway",
-             " Darke",
-             " Mason",
-             " Union"
-          ],
-          "ends_utc":"2020-10-17T13:00:00",
-          "effective_local":"2020-10-16T14:17:00",
-          "onset_utc":"2020-10-17T06:00:00",
-          "expires_local":"2020-10-16T22:30:00",
-          "expires_utc":"2020-10-17T02:30:00",
-          "ends_local":"2020-10-17T09:00:00",
-          "uri":"https:\/\/api.weather.gov\/alerts\/NWS-IDP-PROD-4486346-3727122",
-          "onset_local":"2020-10-17T02:00:00",
-          "effective_utc":"2020-10-16T18:17:00",
-          "severity":"Watch",
-          "title":"Freeze Warning issued October 16 at 2:17PM EDT until October 17 at 9:00AM EDT by NWS Wilmington OH",
-          "description":"* WHAT...Sub-freezing temperatures as low as 30 expected.\n\n* WHERE...Portions of East Central and Southeast Indiana,\nNortheast and Northern Kentucky and Central, South Central,\nSouthwest and West Central Ohio.\n\n* WHEN...From 2 AM to 9 AM EDT Saturday.\n\n* IMPACTS...Frost and freeze conditions will kill crops, other\nsensitive vegetation and possibly damage unprotected outdoor\nplumbing."
-       }
-    ],
-    "city_name":"Dayton",
-    "state_code":"OH"
- }
-]
-const data = weatherObj[0];
+// Import WatchInfo 
+import WatchInfo from "./WatchInfo.jsx";
 
-
+// Will need to change the information depending on the city and state
+// Create function to use for axios request handling 
+// When user picks a location, the weather report should get information for what's going on in the area
 const WatchOut = () => {
-    return (
-        <div className="watch-container">
-            <div className="weather-alerts">
-                <div className="weather-information">
-                    <div className="weather-headers">
-                        <h1>Weather Report</h1>
-                    </div>
-                    <div className="weather-details">
-                        {console.log('TESTING', data)}
-                        <div className="city-name">City Name: {data.city_name}</div>
-                        <div className="country-code">Country Code: {data.country_code}</div>
-                        <div className="state-code">State Code: {data.state_code}</div>
-                        <div className="coordinates">Coordinates
-                           <div className="latitude/longitude">Lat: {data.lat} | Lon: {data.lon}</div>
-                        </div>
-                        <div className="timezone">Timezone: {data.timezone}</div>
-                        <div className="weather-report">
-                            <div className="title">
-                                <h2>Situation Breakdown</h2>
-                            </div>
-                            <div className="breakdown-details">
-                                <p>{data.alerts[0].title}</p>
-                                <p>{data.alerts[0].description}</p>
-                            </div>
-                            Severity level: {data.alerts[0].severity}
-                        </div>
-                    </div>
-                </div>
-            </div>
+    const [report, setReport] = useState(null);
+
+    const getWeather = () => {}
+
+  return (
+    <div className="watch-container">
+      <div className="weather-information">
+        <div className="weather-headers">
+          <h1>Weather Report</h1>
         </div>
-    )
-}
+        <WatchInfo />
+      </div>
+    </div>
+  );
+};
 
 export default WatchOut;
