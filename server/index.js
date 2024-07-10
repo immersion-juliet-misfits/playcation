727cdff9a688f24f0e02524331d899f5165ac32e
TreMartin-OS <tremartin3003@gmail.com>
2024-07-10 11:27:02 -0500

    Fix Google OAuth and Login working correctly

--- server/index.js
+++ server/index.js
@@ -4,55 +4,50 @@ const session = require('express-session');
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth2').Strategy;
 const bodyParser = require('body-parser');
-const isAuthenticated = require('./middleware/auth')
-
+const isAuthenticated = require('./middleware/auth');
+const { User } = require('./db/index'); 
 require('dotenv').config();
 
 // Constants
 // Port
 const PORT = 8000;
-<<<<<<< HEAD
-// Google Places Map keys from .env
-// const gpmKEY = process.env.GOOGLE_MAPS_API_KEY;
-
-
-=======
 // GOOGLE CLIENT keys from .env
 const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
 const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET;
-
 
 // authorize user
 const userAuth = (req, accessToken, refreshToken, profile, done) => {
   // if user has been created
   User.findOne({
     where: {
-      googleId: profile.id
-    }
-  })
-  .then((user) => {
+      googleId: profile.id,
+    },
+  }).then((user) => {
     // if user, log the user in
-    if(user){
-      done(null, user)
+    if (user) {
+      done(null, user);
     } else {
-       User.create({
-        googleId: profile.id, 
-        username: profile.given_name
-    })
-    .then(() => done(null, newUser)) // log in user
+      User.create({
+        googleId: profile.id,
+        username: profile.given_name,
+      })
+        .then((newUser) => done(null, newUser))
+        .then((newUser) => console.log(newUser));
+      // log in user
     }
-  })
+  });
 };
->>>>>>> 47a101fc766356244b95ad9e4504c289393a1cad
 
 // Start Express app
 const app = express();
 // Express session
-app.use(session({
-  secret: 'secret',
-  resave: false,
-  saveUninitialized: true
-}))
+app.use(
+  session({
+    secret: 'secret',
+    resave: false,
+    saveUninitialized: true,
+  })
+);
 // initialize passport
 app.use(passport.initialize());
 app.use(passport.session());
@@ -64,16 +59,20 @@ passport.use(
       clientID: GOOGLE_CLIENT_ID,
       clientSecret: GOOGLE_CLIENT_SECRET,
       callbackURL: `/auth/google/callback`,
-      passReqToCallback: true
+      passReqToCallback: true,
     },
-    userAuth,
-  ),
+    userAuth
+  )
 );
 
 // user info from login
-passport.serializeUser((user, done) => {done(null, user);})
+passport.serializeUser((user, done) => {
+  done(null, user);
+});
 // user info saved from the serializeUser invocation
-passport.deserializeUser((user, done) => {done(null, user);})
+passport.deserializeUser((user, done) => {
+  done(null, user);
+});
 
 // Serves to the dist folder
 app.use(express.static(path.join(__dirname, '../client/dist')));
@@ -81,29 +80,35 @@ app.use(express.static(path.join(__dirname, '../client/dist')));
 app.use(bodyParser.json());
 
 // login with goggle
-app.get('auth/google', passport.authenticate('google', {
-  scope: ['email', 'profile']
-}));
+app.get(
+  '/auth/google',
+  passport.authenticate('google', {
+    scope: ['email', 'profile'],
+  })
+);
 
 // After login, redirect to home for success and / for failure
-app.get('/auth/google/callback', passport.authenticate('google', {
-  successRedirect: '/home',
-  failureRedirect: '/home'
-}));
+app.get(
+  '/auth/google/callback',
+  passport.authenticate('google', {
+    successRedirect: '/home',
+    failureRedirect: '/home',
+  })
+);
 
-// Logout 
+// Logout
 app.post('/auth/logout', (req, res, next) => {
   req.logout((err) => {
     console.error(err);
-  })
+  });
 });
 
-// All wildcard routes 
+// All wildcard routes
 app.get('*', isAuthenticated, (req, res) => {
   res.sendFile(path.join(__dirname, '../client', 'dist', 'index.html'));
 });
 
 // Server listen
 app.listen(PORT, () => {
-  console.info(`Server listening on http://localhost:${PORT}`); 
+  console.info(`Server listening on http://localhost:${PORT}`);
 });
--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -1,6 +1,6 @@
 const isAuthenticated = (req, res, next) => { // check user authentication
 //  if user, go to next, else redirect
-  req.user ? next() : res.redirect('/');
+  !req.user ? res.redirect('/') : next();
 };
 
 module.exports = isAuthenticated;
