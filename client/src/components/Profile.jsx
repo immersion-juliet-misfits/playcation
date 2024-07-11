cb8e41a6b56b17cc260dee598ef6b4933f31d711
jrob112 <josh.roberts0527@gmail.com>
2024-07-11 14:45:38 -0500

    started crud operations

--- client/src/components/Profile.jsx
+++ client/src/components/Profile.jsx
@@ -1,6 +1,38 @@
 import React from 'react';
+import axios from 'axios';
 
 const Profile = () => {
+
+  const getProfile = () => {
+    axios.get()
+    .then(() => {
+
+    })
+    .catch((err) => {
+      console.error('Could not GET Profile: ', err);
+    });
+  };
+
+  const editProfile = () => {
+    axios.patch()
+    .then(() => {
+
+    })
+    .catch((err) => {
+      console.error('Could not edit Profile: ', err);
+    });
+  };
+
+  const deleteProfile = () => {
+    axios.delete()
+    .then(() => {
+
+    })
+    .catch((err) => {
+      console.error('Could not delete Profile: ', err);
+    });
+  }
+  
   return (
     <>
       <h1>Profile</h1>
