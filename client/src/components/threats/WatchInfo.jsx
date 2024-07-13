010934e4e7c29582d2b193dcf1d1fb21141a1bb1
Darius <kudoreha9@gmail.com>
2024-07-13 16:15:26 -0400

    Replaced fake data with real data, and set conditions for sepcific properites being empty or filled.

--- client/src/components/threats/WatchInfo.jsx
+++ client/src/components/threats/WatchInfo.jsx
@@ -69,33 +69,33 @@ const weatherObj = [{
 ]
 const data = weatherObj[0];
 // Remember to use react hooks when revising
-const WatchInfo = () => {
+const WatchInfo = (props) => {
+  const { weatherData } = props;
     return (
         <div className="weather-details">
-          {console.log("TESTING", data)}
-          <div className="city-name">City Name: {data.city_name}</div>
-          <div className="country-code">Country Code: {data.country_code}</div>
-          <div className="state-code">State Code: {data.state_code}</div>
+          <div className="city-name">City Name: {weatherData.city_name}</div>
+          <div className="country-code">Country Code: {weatherData.country_code}</div>
+          <div className="state-code">State Code: {weatherData.state_code}</div>
           <div className="coordinates">
             Coordinates
             <div className="latitude/longitude">
-              Lat: {data.lat} | Lon: {data.lon}
+              Lat: {weatherData.lat} | Lon: {weatherData.lon}
             </div>
           </div>
-          <div className="timezone">Timezone: {data.timezone}</div>
+          <div className="timezone">Timezone: {weatherData.timezone}</div>
           <div className="weather-report">
             <div className="title">
               <h2>Situation Breakdown</h2>
             </div>
             <div className="breakdown-details">
-              <p>{data.alerts[0].title}</p>
-              <p>{data.alerts[0].description}</p>
+              <p>{weatherData.alerts.length === 1 ? weatherData.alerts[0].title : 'No title to report!'}</p>
+              <p>{weatherData.alerts.length === 1 ? weatherData.alerts[0].description : 'Nothing to describe!'}</p>
             </div>
-            Severity level: {data.alerts[0].severity}
+            <p>Severity level: {weatherData.alerts.length === 1 ? weatherData.alerts[0].severity : 'No severity levels!'}</p>
           </div>
           <div className="regions">
             Regions in this area:
-            <div>{data.alerts[0].regions}</div>
+            <div>{weatherData.alerts.length === 1 ? weatherData.alerts[0].regions : 'No regions to report!'}</div>
             </div>
         </div>
     );
